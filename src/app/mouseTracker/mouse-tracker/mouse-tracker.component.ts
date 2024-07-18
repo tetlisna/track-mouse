3eb176fb1c3cbcc842be5c6030d068d332f24303
Tetiana Lisne <tplovic@gmail.com>
2024-07-18 12:20:22 +0300

    init: setup linters, main and child components

---
+++ src/app/mouseTracker/mouse-tracker/mouse-tracker.component.ts
@@ -0,0 +1,32 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Output,
+} from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { distinctUntilChanged, map, throttleTime } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-mouse-tracker',
+  standalone: true,
+  imports: [],
+  templateUrl: './mouse-tracker.component.html',
+  styleUrl: './mouse-tracker.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MouseTrackerComponent {
+  @Output() coordinates = new EventEmitter<{ x: number; y: number }>();
+
+  constructor() {
+    fromEvent<MouseEvent>(document, 'mousemove')
+      .pipe(
+        throttleTime(100),
+        map((event: MouseEvent) => ({ x: event.clientX, y: event.clientY })),
+        distinctUntilChanged(
+          (prev, curr) => prev.x === curr.x && prev.y === curr.y
+        )
+      )
+      .subscribe(coords => this.coordinates.emit(coords));
+  }
+}
